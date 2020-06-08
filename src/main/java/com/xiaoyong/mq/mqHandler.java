f3ebbd27cd60a7bcbce2b6076d14442f1f9b1946
xiao0610 <24844845@qq.com>
2020-06-08 09:38:34 +0800

    冲突

--- src/main/java/com/xiaoyong/mq/mqHandler.java
+++ src/main/java/com/xiaoyong/mq/mqHandler.java
@@ -17,6 +17,6 @@ public class mqHandler {
         System.out.println(message);
         System.out.println(message);
         System.out.println(message);
-        //创建冲突1111
+        //创建冲突
     }
 }
