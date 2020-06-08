0495e1f82e45d920491918b44f1c3d8136625e38
xiao0610 <24844845@qq.com>
2020-06-08 10:17:41 +0800

    创建冲突4

--- src/main/java/com/xiaoyong/mq/mqHandler.java
+++ src/main/java/com/xiaoyong/mq/mqHandler.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 public class mqHandler {
     @RabbitHandler
     public void rabbitHandler(String message) {
-        //创建冲突12
+        //创建冲突55
         System.out.println(message);
         System.out.println(message);
         System.out.println(message);
